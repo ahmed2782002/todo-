36395c2fe3bc87f4d6a789aa6bb36c0332d4070b
Ahmed <modyesam3738@gmail.com>
2024-08-18 04:39:35 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'core/provider/prefs_helper.dart';
 import 'core/theme/application_theme.dart';
 import 'home/home_screen.dart';
 /*
-test github
+test2 github
  */
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
